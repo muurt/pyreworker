8ba226035dc46941bcf9207cdc8d948b0f096cfb
murt <murt.awh@gmail.com>
2022-03-17 17:32:09 +0000

    Fixed import error

--- src/commands/_commandList.ts
+++ src/commands/_commandList.ts
@@ -2,7 +2,6 @@ import { commandInt } from "../interfaces/commandInt";
 import { editbio } from "./editbio";
 import { help } from "./help";
 import { bio } from "./bio";
-import { test } from "./test";
 import { viewbio } from "./viewbio";
 
-export const commandList: commandInt[] = [test, editbio, bio, help, viewbio];
+export const commandList: commandInt[] = [editbio, bio, help, viewbio];
